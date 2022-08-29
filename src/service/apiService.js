037a1a11f0dcaa677a5e3518dba8d4c9d7d3e6bf
Morgan <morgand@tutamail.com>
2022-08-29 11:41:42 +0200

    add api service

---
+++ src/service/apiService.js
@@ -0,0 +1,25 @@
+import { UserModel } from '../models/userModel.js'
+
+export const getUserDatas = (userId) => {
+    return fetch(`http://localhost:3000/user/${userId}`)
+        .then((resp) => resp.json())
+        .then((data) => new UserModel(data));
+};
+
+export const getUserActivity = (userId) => {
+    return fetch(`http://localhost:3000/user/${userId}/activity`)
+        .then((resp) => resp.json())
+        .then((data) => data);
+};
+
+export const getUserSessions = (userId) => {
+    return fetch(`http://localhost:3000/user/${userId}/average-sessions`)
+        .then((resp) => resp.json())
+        .then((data) => data);
+};
+
+export const getUserPerformance = (userId) => {
+    return fetch(`http://localhost:3000/user/${userId}/performance`)
+        .then((resp) => resp.json())
+        .then((data) => data);
+};
