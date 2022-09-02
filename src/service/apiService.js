2deef830614438dbfa0f4dabd08298c6ecc49a08
Morgan <morgand@tutamail.com>
2022-09-02 18:34:44 +0200

    downgrade react v18 -> react v17

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,4 +1,4 @@
-import { UserModel } from '../models/userModel.js'
+import { UserModel } from '../models/userModel.js';
 
 export const getUserDatas = (userId) => {
     return fetch(`http://localhost:3000/user/${userId}`)
