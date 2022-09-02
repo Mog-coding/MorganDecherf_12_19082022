65f8537036084b9decb9bccaabc625c79de31a25
Morgan <morgand@tutamail.com>
2022-09-02 18:16:07 +0200

    add userDatas model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,18 +1,37 @@
+/* UserModel */
 export class UserModel {
     constructor(userJson) {
         if (userJson) {
             this.id = userJson.data.id;
-            this.keyData = userJson.data.keyData;
+            this.keyData = new KeyDataModel(userJson.data.keyData);
             this.todayScore = userJson.data.todayScore || userJson.data.score;
-            this.userInfos = new UserInfoModel(userJson.data.userInfos);
+            this.userInfos = new UserInfosModel(userJson.data.userInfos);
         }
     }
 }
-
-class UserInfoModel {
+class KeyDataModel {
+    constructor(keyDataJSON) {
+        this.calorieCount = keyDataJSON.calorieCount;
+        this.proteinCount = keyDataJSON.proteinCount;
+        this.carbohydrateCount = keyDataJSON.carbohydrateCount;
+        this.lipidCount = keyDataJSON.lipidCount;
+    }
+}
+class UserInfosModel {
     constructor(userInfoJson) {
         this.firstName = userInfoJson.firstName;
         this.lastName = userInfoJson.lastName;
         this.age = userInfoJson.age;
+    }
+}
+
+
+/* AcivityModel */
+export class ActivityModel {
+    constructor(activityJSON) {
+        if (activityJSON) {
+            this.id = activityJSON.id;
+            this.sessions = activityJSON.sessions;
+        }
     }
 }
