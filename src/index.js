f19dabd281e1105541c3609ca5307ee92ad357fe
Morgan <morgand@tutamail.com>
2022-09-12 09:32:10 +0200

    downgrade react v18 -> react v17

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './index.css';
 import DashboardPage from './pages/DashboardPage/DashboardPage';
 import Error404Page from './pages/Error404Page/Error404Page';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
     // <React.StrictMode>
     <Router>
         <Routes>
             <Route path="/:id" element={<DashboardPage />} />
             <Route path="/404" element={<Error404Page />} />
         </Routes>
-    </Router>
-    // </React.StrictMode>
+    </Router>,
+    document.getElementById('root')
 );
