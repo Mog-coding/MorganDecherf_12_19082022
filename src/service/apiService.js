92a68394179a4ecb39a782cf665c797db9c82906
Morgan <morgand@tutamail.com>
2022-09-23 12:09:23 +0200

    updt apiService comms

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -13,57 +13,32 @@ export function getUserDatas(userId) {
         .then(({ data }) => new UserModel(data));
 }
 
-// export const getUserDatas = (userId) => {
-//     return fetch(`http://localhost:3000/user/${userId}`)
-//         .then((resp) => resp.json())
-//         .then(({ data }) => new UserModel(data));
-// };
-
 export function getUserActivity(userId) {
-    const fin = '/activity';
+    const path = '/activity';
     const mocked = '/mockActivity.json';
 
-    return fetch(userId === 'mock' ? `${mocked}` : `${endPoint}${userId}${fin}`)
+    return fetch(userId === 'mock' ? `${mocked}` : `${endPoint}${userId}${path}`)
         .then((resp) => resp.json())
         .then(({ data }) => new ActivityModel(data));
 }
 
-// export const getUserActivity = async (userId) => {
-//     const resp = await fetch(`http://localhost:3000/user/${userId}/activity`)
-//     const { data } = await resp.json();
-//     return new ActivityModel(data)
-// };
-
 export function getUserSessions(userId) {
-    const fin = '/average-sessions';
+    const path = '/average-sessions';
     const mocked = '/mockAverageSession.json';
 
-    return fetch(userId === 'mock' ? `${mocked}` : `${endPoint}${userId}${fin}`)
+    return fetch(userId === 'mock' ? `${mocked}` : `${endPoint}${userId}${path}`)
         .then((resp) => resp.json())
         .then(({ data }) => new AverageSessionsModel(data));
 }
 
-// export const getUserSessions = (userId) => {
-//     return fetch(`http://localhost:3000/user/${userId}/average-sessions`)
-//         .then((resp) => resp.json())
-//         .then(({ data }) => new AverageSessionsModel(data));
-// };
-
 export function getUserPerformance(userId) {
-    const fin = '/performance';
+    const path = '/performance';
     const mocked = '/mockPerformance.json';
 
-    return fetch(userId === 'mock' ? `${mocked}` : `${endPoint}${userId}${fin}`)
+    return fetch(userId === 'mock' ? `${mocked}` : `${endPoint}${userId}${path}`)
         .then((resp) => resp.json())
         .then(({ data }) => new UserPerformance(data.data, data.kind));
 }
-
-// export const getUserPerformance = (userId) => {
-//     return fetch(`http://localhost:3000/user/${userId}/performance`)
-//         .then((resp) => resp.json())
-//         .then(({data}) => {
-//             return new UserPerformance(data.data, data.kind)});
-// };
 
 /**
  * @description
