84a0bbd504ce7fe26a38049bb87907a3ba54f4d5
Morgan <morgand@tutamail.com>
2022-11-01 16:41:53 +0100

    header Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import './Header.css';
-
 import sportSeeIcon from '../../assets/icons/sportSeeIcon.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className="headerHeader">
-            <div>
+            <Link to="/">
                 <img src={sportSeeIcon} alt="" className="sportSeeIcon" />
-            </div>
+            </Link>
             <nav className="headerNav">
                 <p className="accueil">Accueil</p>
                 <p className="profil">Profil</p>
